94cbee05872abc76342790c4bbb21c5a74eb161f
david <davidj4@gmail.com>
2026-04-25 16:02:39 +1000

    drop stale python prototype files

--- start-mpv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Start mpv in idle mode with IPC socket. yt-dlp enables YouTube/etc.
-SOCKET="${MPV_SOCKET:-/tmp/mpv.sock}"
-rm -f "$SOCKET"
-exec mpv --idle=yes --input-ipc-server="$SOCKET" --ytdl=yes --force-window=no --no-terminal "$@"
